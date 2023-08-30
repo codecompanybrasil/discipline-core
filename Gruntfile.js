2af235fe7d084a3cc2ff89f0b578eff6e2fd56d4
Pablo Pereira <pablopereira27@gmail.com>
2023-08-30 20:21:07 -0300

    Inclusão do comando de geração do Bundle CSS
    Inclusão do Grunt para geração do bundle css do projeto (discipline-core.min.css)

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            sitecss: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                },
+                files: {
+                    'src/styles/discipline-core.min.css': [
+                        'src/styles/Bootstrap/bootstrap-reboot.min.css',
+                        'src/styles/Bootstrap/bootstrap-grid.min.css',
+                        'src/styles/discipline-core.css',
+                        'src/components/**/style.css'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['cssmin']);
+};
