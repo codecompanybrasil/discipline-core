a0f9524843e47626321003dff975d323653630a7
Pablo Pereira <pablopereira27@gmail.com>
2023-09-02 14:47:45 -0300

    Implementando conversão SCSS para CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,24 @@
+const sass = require('dart-sass');
+
 module.exports = function (grunt) {
     grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'src/styles/discipline-components.css': ['src/components/**/style.scss']
+                }
+            }
+        },
         cssmin: {
             dist: {
                 files: {
                     'dist/styles/discipline-core.min.css': [
                         'src/styles/discipline-core.css',
-                        'src/components/**/style.css',
+                        'src/styles/discipline-components.css',
                     ],
                 },
             },
@@ -20,8 +33,10 @@ module.exports = function (grunt) {
         },
     })
 
+    // require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('grunt-sass')
     grunt.loadNpmTasks('grunt-contrib-cssmin')
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['cssmin', 'copy'])
+    grunt.registerTask('default', ['sass', 'cssmin', 'copy'])
 }
