983346a26a60fb53c05836e8ec952ebb411e3e24
Pablo Pereira <pablopereira27@gmail.com>
2023-09-02 13:34:58 -0300

    Ajustes no CSS do Discipline Core e substituição do copyfiles
    - Renomeadas variáveis "big" para "lg".
    - Renomeadas variáveis "small" para "sm".
    - Adicionado normalize.css v8.0.1
    - Removido Discipline Reset CSS.
    - Criado Bootstrap Reboot CSS non-minified para checagem e futura remoção.
    - Criado Discipline Reboot CSS.
    - Substituição do "copyfiles" pelo "grunt-contrib-copy"
    - Atualizando Gruntfile para redirecionar bundle css para pasta Dist e copiar Google Fonts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,27 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         cssmin: {
-            sitecss: {
-                options: {
-                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-                },
+            dist: {
                 files: {
-                    'src/styles/discipline-core.min.css': [
+                    'dist/styles/discipline-core.min.css': [
                         'src/styles/discipline-core.css',
                         'src/components/**/style.css',
                     ],
                 },
             },
         },
+        copy: {
+            dist: {
+                files: [
+                    // Copying Google Fonts
+                    { expand: true, cwd: 'src/styles/', src: 'GoogleFonts/**', dest: 'dist/styles/' },
+                ],
+            },
+        },
     })
 
     grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['cssmin'])
+    grunt.registerTask('default', ['cssmin', 'copy'])
 }
