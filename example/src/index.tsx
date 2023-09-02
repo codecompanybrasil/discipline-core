f7b4cb8c720ad333b3569502e906d61cf896a6ff
Pablo Pereira <pablopereira27@gmail.com>
2023-09-02 00:52:48 -0300

    Removendo fontes Lato e Roboto do projeto

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,21 +5,9 @@ import { DcpButton } from '@codecompanybrasil/discipline-core'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <section id="font-examples">
-            <h1>Fontes</h1>
-            <div className="lato">
-                <h2>Lato</h2>
-                <p className='wght-400'>Lato Regular</p>
-                <p className='wght-700'>Lato Bold</p>
-            </div>
-            <hr />
-            <div className="roboto">
-                <h2>Roboto</h2>
-                <p className='wght-400'>Roboto Regular</p>
-                <p className='wght-700'>Roboto Bold</p>
-            </div>
-            <hr />
-            <div className="source-sans-3">
+        <section id='font-examples'>
+            <h1>Fonte</h1>
+            <div className='source-sans-3'>
                 <h2>Source Sans 3</h2>
                 <p className='wght-400'>Source Sans 3 Regular</p>
                 <p className='wght-400 italic'>Source Sans 3 Regular Itálico</p>
@@ -29,7 +17,7 @@ root.render(
             </div>
         </section>
 
-        <section id="buttons">
+        <section id='buttons'>
             <h1>Botões</h1>
             <div>
                 <h2>Botão com cor padrão (primary)</h2>
