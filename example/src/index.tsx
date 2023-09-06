ed2251c8f961661aa6655e9c749c3a08e2df1c60
Pablo Pereira <pablopereira27@gmail.com>
2023-09-06 17:35:06 -0300

    Exemplos atualizados para Discipline Core 1.0.9

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { DcpButton } from '@codecompanybrasil/discipline-core'
+import { DcpButton, DcpLink } from '@codecompanybrasil/discipline-core'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
+        <h1>Discipline Examples</h1>
+        
         <section id='font-examples'>
-            <h1>Fonte</h1>
+            <h2>Fonte</h2>
             <div className='source-sans-3'>
-                <h2>Source Sans 3</h2>
+                <h3>Source Sans 3</h3>
                 <p className='wght-400'>Source Sans 3 Regular</p>
                 <p className='wght-400 italic'>Source Sans 3 Regular Itálico</p>
                 <p className='wght-600'>Source Sans 3 SemiBold</p>
@@ -18,15 +20,28 @@ root.render(
         </section>
 
         <section id='buttons'>
-            <h1>Botões</h1>
+            <h2>Botões</h2>
             <div>
-                <h2>Botão com cor padrão (primary)</h2>
-                <DcpButton text='Enviar' />
+                <h3>Botão com cor padrão (primary)</h3>
+                <DcpButton text='Enviar' onClick={() => confirm('Tem certeza que deseja continuar?')} />
             </div>
             <hr />
             <div>
-                <h2>Botão com cor definida (accent color)</h2>
-                <DcpButton text='Enviar' color='accent' />
+                <h3>Botão com cor definida (accent color)</h3>
+                <DcpButton text='Enviar' color='accent' onClick={() => alert('Teste de click em button')} />
+            </div>
+        </section>
+
+        <section id='link'>
+            <h2>Links</h2>
+            <div>
+                <h3>Botão com cor padrão (primary)</h3>
+                <DcpLink text='Link externo com target blank' href='https://www.google.com/' target='_blank' />
+            </div>
+            <hr />
+            <div>
+                <h3>Botão com cor definida (accent color)</h3>
+                <DcpLink text='Link com Onclick' color='accent' onClick={() => alert('Teste de click em link')} />
             </div>
         </section>
     </React.StrictMode>,
