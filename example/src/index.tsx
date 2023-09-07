b16aba496e0e2207c527f8d87c79720965a1f650
Pablo Pereira <pablopereira27@gmail.com>
2023-09-07 15:00:09 -0300

    Atualizando Exemplos
    Incluindo exemplos de DcpButton e DcpLink com ícones nos slots Start e End.

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { DcpButton, DcpLink } from '@codecompanybrasil/discipline-core'
+import { DcpButton, DcpIcon, DcpLink } from '@codecompanybrasil/discipline-core'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <h1>Discipline Examples</h1>
-        
+
         <section id='font-examples'>
             <h2>Fonte</h2>
             <div className='source-sans-3'>
@@ -23,25 +23,72 @@ root.render(
             <h2>Botões</h2>
             <div>
                 <h3>Botão com cor padrão (primary)</h3>
-                <DcpButton text='Enviar' onClick={() => confirm('Tem certeza que deseja continuar?')} />
+                <DcpButton
+                    text='Enviar'
+                    onClick={() => confirm('Tem certeza que deseja continuar?')} />
             </div>
             <hr />
             <div>
                 <h3>Botão com cor definida (accent color)</h3>
-                <DcpButton text='Enviar' color='accent' onClick={() => alert('Teste de click em button')} />
+                <DcpButton
+                    text='Enviar'
+                    color='accent'
+                    onClick={() => alert('Teste de click em button')} />
+            </div>
+            <hr />
+            <div>
+                <h3>Botão com cor definida (accent color) com ícone no slot start</h3>
+                <DcpButton
+                    text='Enviar'
+                    color='accent'
+                    onClick={() => alert('Teste de click em button')}
+                    slotStart={<DcpIcon.Filter />} />
+            </div>
+            <hr />
+            <div>
+                <h3>Botão com cor definida (accent color) com ícone no slot end</h3>
+                <DcpButton
+                    text='Enviar'
+                    color='accent'
+                    onClick={() => alert('Teste de click em button')}
+                    slotEnd={<DcpIcon.Filter />} />
             </div>
         </section>
 
         <section id='link'>
             <h2>Links</h2>
             <div>
-                <h3>Botão com cor padrão (primary)</h3>
-                <DcpLink text='Link externo com target blank' href='https://www.google.com/' target='_blank' />
+                <h3>Link com cor padrão (primary) e target _blank</h3>
+                <DcpLink
+                    text='Link externo com target blank'
+                    href='https://www.google.com/'
+                    target='_blank' />
             </div>
             <hr />
             <div>
-                <h3>Botão com cor definida (accent color)</h3>
-                <DcpLink text='Link com Onclick' color='accent' onClick={() => alert('Teste de click em link')} />
+                <h3>Link com cor definida (accent color)</h3>
+                <DcpLink
+                    text='Link com Onclick'
+                    color='accent'
+                    onClick={() => alert('Teste de click em link')} />
+            </div>
+            <hr />
+            <div>
+                <h3>Link com cor definida (accent color) com ícone no slot start</h3>
+                <DcpLink
+                    text='Enviar'
+                    color='accent'
+                    onClick={() => alert('Teste de click em button')}
+                    slotStart={<DcpIcon.Filter />} />
+            </div>
+            <hr />
+            <div>
+                <h3>Link com cor definida (accent color) com ícone no slot end</h3>
+                <DcpLink
+                    text='Enviar'
+                    color='accent'
+                    onClick={() => alert('Teste de click em button')}
+                    slotEnd={<DcpIcon.Filter />} />
             </div>
         </section>
     </React.StrictMode>,
