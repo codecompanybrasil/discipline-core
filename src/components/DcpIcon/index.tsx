e8af4aded195bb609b287fba246c9a418147554b
Pablo Pereira <pablopereira27@gmail.com>
2023-09-08 15:35:55 -0300

    Incluídos novos ícones
    - ícone de Menu (apenas na versão preta)
    - Ícones Taskboard e Puzzle nas versões branco e preto.

--- src/components/DcpIcon/index.tsx
+++ src/components/DcpIcon/index.tsx
@@ -7,14 +7,30 @@ import firstPage from '../../../assets/icons/black/icons8-double-left-100.png'
 import firstPageDisabled from '../../../assets/icons/gray/icons8-double-left-100.png'
 import lastPage from '../../../assets/icons/black/icons8-double-right-100.png'
 import lastPageDisabled from '../../../assets/icons/gray/icons8-double-right-100.png'
+import menu from '../../../assets/icons/black/icons8-more-100.png'
 import next from '../../../assets/icons/black/icons8-forward-100.png'
 import nextDisabled from '../../../assets/icons/gray/icons8-forward-100.png'
+import puzzleBlack from '../../../assets/icons/black/icons8-puzzle-100.png'
+import puzzleWhite from '../../../assets/icons/white/icons8-puzzle-100.png'
+import taskBoardBlack from '../../../assets/icons/black/icons8-tarefa-100.png'
+import taskBoardWhite from '../../../assets/icons/white/icons8-tarefa-100.png'
 
-interface DcpIconAttributes extends React.ImgHTMLAttributes<HTMLImageElement> {}
+interface DcpIconAttributes extends React.ImgHTMLAttributes<HTMLImageElement> {
+    color?: 'black' | 'white'
+}
 
 const DcpIcon = (props: DcpIconAttributes) => <img className='dcp-icon' {...props} />
 
+const Puzzle = ({ color = 'white', ...props }: DcpIconAttributes) => {
+    return <DcpIcon src={color == 'white' ? puzzleWhite : puzzleBlack} {...props} />
+}
+const Taskboard = ({ color = 'white', ...props }: DcpIconAttributes) => {
+    return <DcpIcon src={color == 'white' ? taskBoardWhite : taskBoardBlack} {...props} />
+}
+
 const Filter = (props: DcpIconAttributes) => <DcpIcon src={filter} {...props} />
+const Menu = (props: DcpIconAttributes) => <DcpIcon src={menu} {...props} />
+
 const Back = (props: DcpIconAttributes) => <DcpIcon src={back} {...props} />
 const BackDisabled = (props: DcpIconAttributes) => <DcpIcon src={backDisabled} {...props} />
 const Next = (props: DcpIconAttributes) => <DcpIcon src={next} {...props} />
@@ -25,7 +41,10 @@ const LastPage = (props: DcpIconAttributes) => <DcpIcon src={lastPage} {...props
 const LastPageDisabled = (props: DcpIconAttributes) => <DcpIcon src={lastPageDisabled} {...props} />
 
 export {
+    Puzzle,
+    Taskboard,
     Filter,
+    Menu,
 
     // Pagination
     Back,
