2b82b7000180fb78111782d5e410869acc4eeb26
phgodoycosta <phgodoycosta@gmail.com>
2023-11-11 19:16:57 -0400

    prepare para o release 1.0.20

--- src/components/DcpButton/index.tsx
+++ src/components/DcpButton/index.tsx
@@ -9,7 +9,6 @@ interface DcpButtonAttribtutes extends React.ButtonHTMLAttributes<HTMLButtonElem
     slotstart?: any
     slotend?: any
     className?: string
-    type: string | undefined
     fontSize?: string
     border?: string
 }
@@ -38,9 +37,9 @@ const DcpButton = ({
     const cssClasses = ['dcp-button', color, props.className].join(' ')
 
     return (
-        <Tag {...props} type={type} className={cssClasses} style={{borderWidth: border}}>
+        <Tag {...props} type={type} className={cssClasses} style={{ borderWidth: border }}>
             {props.slotstart && <span className='slot-start'>{props.slotstart}</span>}
-            <span style={{fontSize: fontSize}} >{text}</span>
+            <span style={{ fontSize: fontSize }}>{text}</span>
             {props.slotend && <span className='slot-end'>{props.slotend}</span>}
         </Tag>
     )
