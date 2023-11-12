a15a2ebb6aa48a38c5c5875562e789f79a8efc6d
phgodoycosta <phgodoycosta@gmail.com>
2023-11-12 15:20:27 -0400

    Adicionando DcpLogo para utilizar as logos do Discipline

---
+++ src/components/DcpLogo/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import discipline_baseline from '../../assets/logos/discipline_baseline.png'
+import logo_discipline_trans_azul from '../../assets/logos/logo_discipline_trans_azul.png'
+import logo_discipline_trans_preto from '../../assets/logos/logo_discipline_trans_preto.png'
+import logo_discipline from '../../assets/logos/logo_discipline.png'
+import logo_sem_padding from '../../assets/logos/logo_sem_padding.png'
+import logo_trans from '../../assets/logos/logo_trans.png'
+import logo from '../../assets/logos/logo.png'
+
+interface DcpLogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+    width?: string
+    height?: string
+}
+
+const DcpLogo = (props: DcpLogoProps) => {
+    return (
+        <img className='dcp-logo' style={{ width: props.width ?? 'auto', height: props.height ?? 'auto' }} {...props} />
+    )
+}
+
+const Logo = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo} width={width} height={height} {...props} />
+}
+
+const DisciplineBaseline = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={discipline_baseline} width={width} height={height} {...props} />
+}
+
+const LogoDisciplineTransAzul = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo_discipline_trans_azul} width={width} height={height} {...props} />
+}
+
+const LogoDisciplineTransPreto = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo_discipline_trans_preto} width={width} height={height} {...props} />
+}
+
+const LogoDiscipline = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo_discipline} width={width} height={height} {...props} />
+}
+
+const LogoSemPadding = ({ width = '100px', height = '100px', ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo_sem_padding} width={width} height={height} {...props} />
+}
+
+const LogoTrans = ({ width, height, ...props }: DcpLogoProps) => {
+    return <DcpLogo src={logo_trans} width={width} height={height} {...props} />
+}
+
+export {
+    Logo,
+    DisciplineBaseline,
+    LogoDisciplineTransAzul,
+    LogoDisciplineTransPreto,
+    LogoDiscipline,
+    LogoSemPadding,
+    LogoTrans,
+}
--- testabble-app/src/App.tsx
+++ testabble-app/src/App.tsx
@@ -2,18 +2,32 @@ import "./index.css";
 import DcpButton from "./components/DcpButton";
 import { Puzzle, Menu, ExportDocument, Google } from "./components/DcpIcon";
 import DcpIconButton from "./components/DcpIconButton";
+import { Logo, DisciplineBaseline, LogoDiscipline, LogoDisciplineTransAzul, LogoDisciplineTransPreto, LogoSemPadding, LogoTrans } from "./components/DcpLogo";
 
 function App() {
 
     return (
         <>
-            <DcpButton text="Teste" type="primary" fontSize="20px" />
+            <DcpButton text="Teste" type="accent" fontSize="20px" />
             <Puzzle color="black" width={50} />
             <DcpIconButton color="primary">
                 <Menu width={50} color="white" />
             </DcpIconButton>
             <ExportDocument color="black" />
             <Google />
+            <Logo width="100px" height="100px" />
+            <br />
+            <DisciplineBaseline />
+            <br />
+            <LogoDiscipline />
+            <br />
+            <LogoDisciplineTransAzul />
+            <br />
+            <LogoDisciplineTransPreto />
+            <br />
+            <LogoTrans />
+            <br />
+            <LogoSemPadding />
         </>
     );
 }
